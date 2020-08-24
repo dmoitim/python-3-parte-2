76dd51f6ce15a663b60703cda19cb4a9669a21cf
Devair Luiz Moitim <dmoitim@gmail.com>
2020-08-24 20:56:15 -0300

    Calcula enforcamento ou acerto

--- forca.py
+++ forca.py
@@ -3,27 +3,41 @@ def jogar():
     print("* Bem vindo ao jogo da Forca! *")
     print("*******************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "BANANA"
     letras_acertadas = ["_", "_", "_", "_", "_", "_"]
 
     enforcou = False
     acertou = False
+    tentativas = 0
 
     print(letras_acertadas)
+    letras_faltando = str(letras_acertadas.count("_"))
 
     while (not enforcou and not acertou):
         chute = input("Qual letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        posicao = 0
-        for letra in palavra_secreta:
-            if (chute.upper() == letra.upper()):
-                letras_acertadas[posicao] = letra
-                letras_faltando = str(letras_acertadas.count("_"))
-            posicao = posicao + 1
+        if (chute in palavra_secreta):
+            posicao = 0
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[posicao] = letra
+                    letras_faltando = int(letras_acertadas.count("_"))
+                posicao += 1
+        else:
+            tentativas += 1
+            print("Ops, você errou! Faltam {} tentativas.".format(6-tentativas))
+
+        enforcou = tentativas == 6
+        acertou = letras_faltando == 0
 
         print(letras_acertadas)
         print("Ainda falta acertar {} letras.".format(letras_faltando))
+
+    if (acertou):
+        print("Você ganhou!!!")
+    else:
+        print("Você perdeu.")
 
     print("Fim do jogo!")
 
